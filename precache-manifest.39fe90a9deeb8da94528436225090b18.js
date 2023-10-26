5c673409a0d52539ca248caa1feb611515a26924
praveen <praveenkumargoud838@gmail.com>
2023-10-26 20:28:01 +0530

    Updates

---
+++ precache-manifest.39fe90a9deeb8da94528436225090b18.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d8f7de798cb8831d1c11a1086dafd67",
+    "url": "/Bookmystay/index.html"
+  },
+  {
+    "revision": "61e022502eb7a6dbb0fc",
+    "url": "/Bookmystay/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8310af297f3c455f1e7f",
+    "url": "/Bookmystay/static/css/main.bce79baf.chunk.css"
+  },
+  {
+    "revision": "61e022502eb7a6dbb0fc",
+    "url": "/Bookmystay/static/js/2.cb1b40e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Bookmystay/static/js/2.cb1b40e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8310af297f3c455f1e7f",
+    "url": "/Bookmystay/static/js/main.9bd0642c.chunk.js"
+  },
+  {
+    "revision": "6ecc4aa6964cb9513514",
+    "url": "/Bookmystay/static/js/runtime-main.3428bc79.js"
+  },
+  {
+    "revision": "5ef674f8971a6848b4ef9d491af90996",
+    "url": "/Bookmystay/static/media/backgrnd.5ef674f8.jpg"
+  },
+  {
+    "revision": "4df1bf95e5979b944cbdb06633c3a61e",
+    "url": "/Bookmystay/static/media/waterback.4df1bf95.jpg"
+  }
+]);
